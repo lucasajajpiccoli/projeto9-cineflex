8a8f3aca628f888732237d13cf9ee4311b7114ae
lucasajajpiccoli <lucasajaj@gmail.com>
2022-09-11 11:29:42 -0300

    Correção de erro de digitação

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,4 +1,4 @@
-import { Fgrament, useState, useEffect } from 'react';
+import { Fragment, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import styled from 'styled-components';
